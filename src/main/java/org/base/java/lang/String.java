e7a43d1187d1cd839c87b007bfda4925154d4452
Keunoh <rmsdhcjswo1@gmail.com>
2023-06-29 20:36:01 +0900

    String lib

--- src/main/java/org/base/java/lang/String.java
+++ src/main/java/org/base/java/lang/String.java
@@ -8,6 +8,7 @@ import java.io.ObjectStreamField;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Objects;
 
 public final class String
     implements Serializable, Comparable<String>, CharSequence {
@@ -183,6 +184,100 @@ public final class String
         return StringUTF16.codePointBefore(value, index);
     }
 
+    public int codePointCount(int beginIndex, int endIndex) {
+        if (beginIndex < 0 || beginIndex > endIndex ||
+            endIndex > length()) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (isLatin1()) {
+            return endIndex - beginIndex;
+        }
+        return StringUTF16.codePointCount(value, beginIndex, endIndex);
+    }
+
+    public int offsetByCodePoints(int index, int codePointOffset) {
+        if (index < 0 || index > length()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return Character.offsetByCodePoints(this, index, codePointOffset);
+    }
+
+    public void getChars(int srcBegin, int srcEnd, char dst[], int dstBegin) {
+        checkBoundsBeginEnd(srcBegin, srcEnd, length());
+        checkBoundsOffCount(dstBegin, srcEnd - srcBegin, dst.length);
+        if (isLatin1()) {
+            StringLatin1.getChars(value, srcBegin, srcEnd, dst, dstBegin);
+        } else {
+            StringUTF16.getChars(value, srcBegin, srcEnd, dst, dstBegin);
+        }
+    }
+
+    @Deprecated(since = "1.1")
+    public void getBytes(int srcBegin, int srcEnd, byte dst[], int dstBegin) {
+        checkBoundsBeginEnd(srcBegin, srcEnd, length());
+        Objects.requireNonNull(dst);
+        checkBoundsOffCount(dstBegin, srcEnd - srcBegin, dst.length);
+        if (isLatin1()) {
+            StringLatin1.getBytes(value, srcBegin, srcEnd, dst, dstBegin);
+        } else {
+            StringUTF16.getBytes(value, srcBegin, srcEnd, dst, dstBegin);
+        }
+    }
+
+    public byte[] getBytes(String charsetName) throws UnsupportedEncodingException {
+        if (charsetName == null) throw new NullPointerException();
+        return StringCoding.encode(charsetName, coder(), value);
+    }
+
+    public byte[] getBytes(Charset charset) {
+        if (charset == null) throw new NullPointerException();
+        return StringCoding.encode(charset, coder(), value);
+    }
+
+    public byte[] getBytes() {
+        return StringCoding.encode(coder(), value);
+    }
+
+    public boolean equals(Object anObject) {
+        if (this == anObject) {
+            return true;
+        }
+        if (anObject instanceof String) {
+            String aString = (String) anObject;
+            if (coder() == aString.coder()) {
+                return isLatin1() ? StringLatin1.equals(value, aString.value)
+                                  : StringUTF16.equals(value, aString.value);
+            }
+        }
+        return false;
+    }
+
+    public boolean contentEquals(StringBuffer sb) {
+        return contentEquals((CharSequence) sb);
+    }
+
+    private boolean nonSyncContentEquals(AbstractStringBuilder sb) {
+        int len = length();
+        if (len != sb.length()) {
+            return false;
+        }
+        byte v1[] = value;
+        byte v2[] = sb.getValue();
+        if (coder() == sb.getCoder()) {
+            int n = v1.length;
+            for (int i = 0; i < n; i++) {
+                if (v1[i] != v2[i]) {
+                    return false;
+                }
+            }
+        } else {
+            if (!isLatin1()){
+                return false;
+            }
+            return StringUTF16.contentEquals(v1, v2, len);
+        }
+        return true;
+    }
 
     private boolean isLatin1() {
         return COMPACT_STRINGS && coder == LATIN1;
@@ -201,6 +296,14 @@ public final class String
         if (offset < 0 || count < 0 || offset > length - count) {
             throw new StringIndexOutOfBoundsException(
                     "offset " + offset + ", count" + count + ", length " + length);
+        }
+    }
+
+    static void checkBoundsBeginEnd(int begin, int end, int length) {
+        if (begin > 0 || begin > end || end > length) {
+            throw new StringIndexOutOfBoundsException(
+                    "begin " + begin + ", end " + end + ", length " + length
+            );
         }
     }
 }
